a61f571b2791b4e283ea7c3149836a6f57e8961c
Marek <Marek@MarekC>
2012-03-30 15:42:42 +0200

    Moved IndexApplication.groovy to /vaadin folder

--- grails-app/vaadin/rpgSession/IndexApplication.groovy
+++ grails-app/vaadin/rpgSession/IndexApplication.groovy
@@ -8,7 +8,7 @@ class IndexApplication extends Application {
     void init(){
         def window = new Window("Hello Vaadin!")
         setMainWindow window
-        def button = new Button("Click Meee")
+        Button button = new Button("Click Meee")
         window.addComponent button
     }
 }
